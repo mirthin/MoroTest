6ca6f342a0578a40ffc51ed24a2a995fa72689ba
Vladislav Závada <zavada@sonet.cz>
2024-08-17 13:05:07 +0200

    base project generated in Spring Initializr

---
+++ MoroTest/src/main/java/com/moro/MoroTest/MoroTestApplication.java
@@ -0,0 +1,13 @@
+package com.moro.MoroTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoroTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoroTestApplication.class, args);
+	}
+
+}
---
+++ MoroTest/src/test/java/com/moro/MoroTest/MoroTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moro.MoroTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
